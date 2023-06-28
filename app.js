cd8cab49edf2a55f7853374ee991210065df2b7d
Jason McGrath <jnomad21@yahoo.com>
2023-06-27 17:39:02 -0700

    scramble word functionality

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const words =['crocodile', 'alligator', 'giraffe', 'octopus', 'tarantula']
-
 const randWord = words[Math.floor(Math.random()*words.length)]
+const newWord = Array.from(randWord)
 
-console.log(randWord)
-
-const wordScrambler = function (){
-
+const shuffle = function(){
+    for (let i=0; i<newWord.length; i++){
+        let temp = newWord[i];
+        let r = Math.floor(Math.random()*newWord.length);
+        newWord[i]=newWord[r]
+        newWord[r]=temp
+        const scramWord = newWord.join("")
+        return scramWord
+    }
 }
+console.log(shuffle())
+    
+
+ 
