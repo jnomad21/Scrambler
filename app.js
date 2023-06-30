414b4c7b21e7825f100257dd48c23fa854333f2b
Jason McGrath <jnomad21@yahoo.com>
2023-06-30 16:06:18 -0700

    function now mixes in new words with each event click

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 'use strict';
 
 const words =['crocodile', 'alligator', 'giraffe', 'octopus', 'tarantula', 'hippopotomus', 'armadillo', 'aardvark', 'kangaroo']
-const randWord = words[Math.floor(Math.random()*words.length)]
-const newWord = Array.from(randWord)
+
 const scramBtn = document.querySelector('#word-button')
 const mixedWord = document.querySelector('#scrambled-word')
 const guessWord = document.querySelector('#guess')
 const submitBtn =document.querySelector('#submit')
-
 const displayMessage=function(message){
     document.querySelector('.message').textContent = message
 
 }
-
-const shuffle = function(){
-    for (let i=0; i<newWord.length; i++){
-        let temp = newWord[i];
-        let r = Math.floor(Math.random()*newWord.length);
-        newWord[i]=newWord[r]
-        newWord[r]=temp
-        const scramWord = newWord.join("")
+const randWord = function(){
+    const word=words[Math.floor(Math.random()*words.length)];
+    const mixedWord = Array.from(word)
+    for (let i=0; i<mixedWord.length; i++){
+        let temp = mixedWord[i];
+        let r = Math.floor(Math.random()*mixedWord.length);
+        mixedWord[i]=mixedWord[r]
+        mixedWord[r]=temp
+        const scramWord = mixedWord.join("")
         return scramWord
     }
 }
 
     
 scramBtn.addEventListener('click', function(){
-shuffle
-mixedWord.textContent=shuffle()
+randWord()
+mixedWord.textContent=randWord()
 })
 
 submitBtn.addEventListener('click', function(){
