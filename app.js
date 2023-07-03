b9daeca48e31eadf4fcc39e77ed3dea3232783ab
Jason McGrath <jnomad21@yahoo.com>
2023-07-03 15:14:13 -0700

    fixed main functionality

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ const guessWord = document.querySelector('#guess')
 const submitBtn =document.querySelector('#submit')
 const displayMessage=function(message){
     document.querySelector('.message').textContent = message
-
 }
+const word=function(){
+    const wor = words[Math.floor(Math.random()*words.length)];
+    return wor
+}
+const newWord=word()
+console.log(newWord)
+
 const randWord = function(){
-    const word=words[Math.floor(Math.random()*words.length)];
-    const mixedWord = Array.from(word)
+    const mixedWord = Array.from(newWord)
     for (let i=0; i<mixedWord.length; i++){
         let temp = mixedWord[i];
         let r = Math.floor(Math.random()*mixedWord.length);
@@ -23,14 +28,13 @@ const randWord = function(){
     }
 }
 
-    
 scramBtn.addEventListener('click', function(){
 randWord()
 mixedWord.textContent=randWord()
 })
 
 submitBtn.addEventListener('click', function(){
-    if(guessWord.value===randWord){
+    if(guessWord.value===newWord){
         displayMessage("YOU GOT IT😎")
     }else{
         displayMessage("BETTER LUCK NEXT TIME, DUMB DUMB😭")
