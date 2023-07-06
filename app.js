2dce92c97550fb821c7c3a5b017360c6f809c218
Jason McGrath <jnomad21@yahoo.com>
2023-07-05 17:06:48 -0700

    refactored functionality to work correctly

--- app.js
+++ app.js
@@ -1,59 +1,57 @@
-'use strict';
+const words = ['crocodile', 'alligator', 'giraffe', 'octopus', 'tarantula', 'hippopotomus', 'armadillo', 'aardvark', 'kangaroo', 'hedgehog', 'elephant', 'albatross', 'orangutan', 'rhinoceros', 'chimpanzee'];
 
-const words =['crocodile', 'alligator', 'giraffe', 'octopus', 'tarantula', 'hippopotomus', 'armadillo', 'aardvark', 
-'kangaroo', 'hedgehog', 'elephant', 'albatross', 'orangutan', 'rhinoceros', 'chimpanzee']
+const scramBtn = document.querySelector('#word-button');
+const mixedWord = document.querySelector('#scrambled-word');
+const guessWord = document.querySelector('#guess');
+const submitBtn = document.querySelector('#submit');
 
-const scramBtn = document.querySelector('#word-button')
-const mixedWord = document.querySelector('#scrambled-word')
-const guessWord = document.querySelector('#guess')
-const submitBtn =document.querySelector('#submit')
-const displayMessage=function(message){
-    document.querySelector('.message').textContent = message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+function randWord(jumbleWord) {
+  for (let i = 0; i < jumbleWord.length; i++) {
+    let temp = jumbleWord[i];
+    let r = Math.floor(Math.random() * jumbleWord.length);
+    jumbleWord[i] = jumbleWord[r];
+    jumbleWord[r] = temp;
+  }
+  return jumbleWord.join('');
 }
-const word=function(){
-    const wor = words[Math.floor(Math.random()*words.length)];
-    return wor
-}
-const newWord=word()
 
-const randWord = function(){
-    const mixedWord = Array.from(newWord)
-    for (let i=0; i<mixedWord.length; i++){
-        let temp = mixedWord[i];
-        let r = Math.floor(Math.random()*mixedWord.length);
-        mixedWord[i]=mixedWord[r]
-        mixedWord[r]=temp
-        const scramWord = mixedWord.join("")
-        if(scramWord !== newWord)
-        return scramWord
-    }
-}
-const submitResponse = function(){
-    if(guessWord.value===newWord){
-        displayMessage("YOU GOT IT😎")
-    }else{
-        displayMessage("BETTER LUCK NEXT TIME, DUMB DUMB😭")
-    }
-}
-// event listeners
-window.addEventListener('load', function(){
-    randWord()
-    mixedWord.textContent=randWord() 
-})
+const word = function () {
+  let wor = words[Math.floor(Math.random() * words.length)];
+  const jumbleWord = Array.from(wor);
+  const scramWord = randWord(jumbleWord);
+  mixedWord.textContent = scramWord;
+  return wor;
+};
 
-scramBtn.addEventListener('click', function(){
-randWord()
-mixedWord.textContent=randWord()
-})
+let newWord = word();
 
-submitBtn.addEventListener('click', function(){
-    submitResponse()
-})
 
-guessWord.addEventListener('keypress', function(e){
-    if (e.keyCode === 13) {
-    submitResponse()
-}
-})
+const submitResponse = function () {
+  if (guessWord.value === newWord) {
+    displayMessage('YOU GOT IT😎');
+  } else {
+    displayMessage('BETTER LUCK NEXT TIME, DUMB DUMB😭');
+  }
+};
 
- 
+window.addEventListener('load', function () {
+  newWord = word();
+});
+
+scramBtn.addEventListener('click', function () {
+  newWord = word();
+});
+
+submitBtn.addEventListener('click', function () {
+  submitResponse();
+});
+
+guessWord.addEventListener('keypress', function (e) {
+  if (e.keyCode === 13) {
+    submitResponse();
+  }
+});
